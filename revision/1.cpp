0ffb37776e716994e516a1b0fb85d83005ee9ca1
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-06-24 11:01:24 +0530

    1.cpp

---
+++ revision/1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        this->data=value;
+        this->next=NULL;
+    }
+};
+
+
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    Node *head= NULL;
+    for(int i=0; i<5; i++) {
+        if(head == NULL) 
+        {
+            head=new Node(arr[i]);
+        }
+        else 
+        {
+            Node *temp;
+            temp=new Node(arr[i]);
+            temp->next=head;
+            head= temp;
+        }
+    }
+    Node *temp = head;
+    while(temp !=NULL) 
+    {   
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
