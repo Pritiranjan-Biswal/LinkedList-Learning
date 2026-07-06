848d42783250405b4c816f95a97abef3be5af180
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-07-06 17:37:41 +0530

    dll

---
+++ double_ll/create_a_linkedlist_from_a_arr.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node
+{
+    public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int value) 
+    {
+        data=value;
+        prev=NULL;
+        next=NULL;
+    }
+};
+
+
+
+int main() {
+    Node *head=NULL;
+    Node *tail=NULL;
+
+    int arr[]={1,2,3,4,5};
+
+    for(int i=0; i<5; i++) {
+        //linkedlist doesn't exist
+        if(head==NULL) 
+        {
+            head=new Node(arr[i]);
+            tail=head;
+        }
+
+        //ll exist
+        else 
+        {
+            Node *temp=new Node(arr[i]);
+            temp->prev=tail;
+            tail->next=temp;
+            tail=temp;
+        }
+       
+
+
+    }
+
+    Node *a=head;
+    while(a) 
+    {
+        cout<<"prev ->"<<a->prev<<" "<< "data ->"<<a->data<< " "<<"next ->"<<a->next<<endl;
+        a=a->next;
+    }
+}
